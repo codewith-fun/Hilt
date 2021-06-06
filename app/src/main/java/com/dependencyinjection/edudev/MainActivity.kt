c602d537bd5f8e6158676ff74ee213e84f952444
darktheft <laxmipalkant01@gmail.com>
2021-06-06 17:09:15 +0530

    added construction Injection

--- app/src/main/java/com/dependencyinjection/edudev/MainActivity.kt
+++ app/src/main/java/com/dependencyinjection/edudev/MainActivity.kt
@@ -2,22 +2,51 @@ package com.dependencyinjection.edudev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.dependencyinjection.edudev.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    var datas: String ="pal G"
-    lateinit var  testt : TextView
+    lateinit var mainBinding : ActivityMainBinding
+
+
+    // field injection
+    @Inject
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        testt = findViewById<TextView>(R.id.maintest)
-        testt?.setOnClickListener {
-            testt.setText("Name is $datas")
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        mainBinding.setLifecycleOwner(this)
+        mainBinding.maintest?.setOnClickListener {
+            mainBinding.maintest.setText("${someClass.doAThing()}\n and \n ${someClass.doSomeOtherThing()}")
         }
         
     }
 
+
+    class SomeClass
+    @Inject
+    constructor(val someOtherClass: SomeOtherClass){
+        fun doAThing():String{
+            var data = "Hi Ms. Laxmi kant pal "
+            return "data is $data"
+        }
+
+        fun doSomeOtherThing():String{
+            return someOtherClass.doSomeOtherThing()
+        }
+
+    }
+
+    class SomeOtherClass
+    @Inject
+    constructor(){
+        fun doSomeOtherThing():String{
+            return "Another from"
+        }
+    }
 
 }
