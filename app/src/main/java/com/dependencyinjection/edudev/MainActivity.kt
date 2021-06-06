8aaa60ffc5d421764b374a53d09c413ffb1e70cd
darktheft <laxmipalkant01@gmail.com>
2021-06-06 16:37:18 +0530

    added

---
+++ app/src/main/java/com/dependencyinjection/edudev/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.dependencyinjection.edudev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    var datas: String ="pal G"
+    lateinit var  testt : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        testt = findViewById<TextView>(R.id.maintest)
+        testt?.setOnClickListener {
+            testt.setText("Name is $datas")
+        }
+        
+    }
+
+
+}
---
+++ app/src/main/java/com/dependencyinjection/edudev/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.dependencyinjection.edudev
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+
+}
